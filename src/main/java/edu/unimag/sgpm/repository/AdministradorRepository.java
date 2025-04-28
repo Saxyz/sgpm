429c19483755e31cc96da5838b77c3d2aff23d01
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-04-28 00:20:16 -0500

    [CREATE] Repositorios de las clases

---
+++ src/main/java/edu/unimag/sgpm/repository/AdministradorRepository.java
@@ -0,0 +1,7 @@
+package edu.unimag.sgpm.repository;
+
+import edu.unimag.sgpm.model.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministradorRepository extends JpaRepository<Administrador, Integer> {
+}

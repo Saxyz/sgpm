c8f947a5553b3ba539b28b0122643bffb15dd759
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-04-29 22:51:50 -0500

    [FIX] Integer estado -> String estado

--- src/main/java/edu/unimag/sgpm/dto/espacio/EspacioDto.java
+++ src/main/java/edu/unimag/sgpm/dto/espacio/EspacioDto.java
@@ -3,6 +3,6 @@ package edu.unimag.sgpm.dto.espacio;
 public record EspacioDto(
         String id,
         Integer parqueadero,
-        Integer estado
+        String estado
 ) {
 }
--- src/main/java/edu/unimag/sgpm/dto/reserva/ReservaDto.java
+++ src/main/java/edu/unimag/sgpm/dto/reserva/ReservaDto.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public record ReservaDto(
         Integer id,
-        Integer estado,
+        String estado,
         Integer usuario,
         String espacio,
         LocalDateTime fecha

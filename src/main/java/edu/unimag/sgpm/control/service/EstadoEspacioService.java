08199bdbf47ce71b4315fa3c6ab6352191bf8140
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-06 15:21:50 -0500

    [CREATE] creando services de clases

---
+++ src/main/java/edu/unimag/sgpm/control/service/EstadoEspacioService.java
@@ -0,0 +1,13 @@
+package edu.unimag.sgpm.control.service;
+
+import edu.unimag.sgpm.control.dto.estadoEspacio.EstadoEspacioDto;
+
+import java.util.List;
+
+public interface EstadoEspacioService {
+    EstadoEspacioDto createEspacio(EstadoEspacioDto estadoEspacioDto);
+    EstadoEspacioDto findEstadoEspacioById(Integer id);
+    List<EstadoEspacioDto> findAllEstadoEspacios();
+    EstadoEspacioDto updateEstadoEspacio(Integer id, EstadoEspacioDto estadoEspacioDto);
+    void deleteEstadoEspacio(Integer id);
+}

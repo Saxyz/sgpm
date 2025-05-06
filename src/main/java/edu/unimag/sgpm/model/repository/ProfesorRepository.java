403aaac6d759b139fffe287a997408fde37916e5
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-06 14:57:43 -0500

    [DELETE] -> Innecesario

--- src/main/java/edu/unimag/sgpm/model/repository/ProfesorRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.unimag.sgpm.model.repository;
-
-import edu.unimag.sgpm.model.entity.Profesor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepository extends JpaRepository<Profesor, Integer> {
-}

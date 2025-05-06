54e85cda31e1c809ef85d7557cab92a3953db09c
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-06 15:04:20 -0500

    [CREATE] creando clases de estados

--- src/main/java/edu/unimag/sgpm/model/entity/EstadoDeEspacio.java
+++ src/main/java/edu/unimag/sgpm/model/entity/EstadoDeEspacio.java
@@ -1,7 +1,22 @@
 package edu.unimag.sgpm.model.entity;
 
-public enum EstadoDeEspacio {
-    Ocupado,
-    Reservado,
-    Vacio
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "EstadosDeEspacio")
+public class EstadoDeEspacio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idEstado;
+
+    @Column(nullable = false)
+    private String nombre;
 }
--- src/main/java/edu/unimag/sgpm/model/entity/EstadoDeReserva.java
+++ src/main/java/edu/unimag/sgpm/model/entity/EstadoDeReserva.java
@@ -1,9 +1,21 @@
 package edu.unimag.sgpm.model.entity;
 
-public enum EstadoDeReserva {
-    Pendiente,
-    Confirmada,
-    Finalizada,
-    Cancelada,
-    Expirada
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "EstadosDeReserva")
+@Builder
+@Data
+public class EstadoDeReserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idEstado;
+
+    @Column(nullable = false)
+    private String nombre;
 }

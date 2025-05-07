f852fb3638ae4d6ac89e05a15f37b263e10cf313
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-07 13:35:37 -0500

    [MOD] agregando nuevos metodos

--- src/main/java/edu/unimag/sgpm/model/repository/UsuarioRepository.java
+++ src/main/java/edu/unimag/sgpm/model/repository/UsuarioRepository.java
@@ -1,16 +1,16 @@
 package edu.unimag.sgpm.model.repository;
 
+import edu.unimag.sgpm.model.entity.Role;
 import edu.unimag.sgpm.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByRoles(Set<Role> roles);
 
-    Optional<Usuario> findUsuarioByIdUsuario(Integer id);
-
-    List<Usuario> findByParqueaderoIdParqueadero(Integer id);
+    Optional<Usuario> findByCorreo(String correo);
 }

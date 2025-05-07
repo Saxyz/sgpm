c840a71e5fcce5f4778fe657e8acad750cc5ddbe
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-07 13:32:04 -0500

    [CREATE] creando clases

---
+++ src/main/java/edu/unimag/sgpm/model/entity/ERole.java
@@ -0,0 +1,5 @@
+package edu.unimag.sgpm.model.entity;
+
+public enum ERole {
+    ROLE_ADMINISTRADOR, ROLE_ESTUDIANTE , ROLE_RolePROFESOR , ROLE_VIGILANTE
+}
---
+++ src/main/java/edu/unimag/sgpm/model/entity/Role.java
@@ -0,0 +1,21 @@
+package edu.unimag.sgpm.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+}

3eb9b4214886dffcb6a329a1a142478081a7c682
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-27 20:39:37 -0500

    [MOD] añadiendo nuevos atributos (imagen, ruta)

--- src/main/java/edu/unimag/sgpm/control/dto/MotoDto.java
+++ src/main/java/edu/unimag/sgpm/control/dto/MotoDto.java
@@ -1,10 +1,14 @@
 package edu.unimag.sgpm.control.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public record MotoDto(
         String idMoto,
         Integer idUsuario,
         Integer idParqueadero,
         String modelo,
-        String descripcion
+        String descripcion,
+        MultipartFile imagen,
+        String ruta
 ) {
 }
--- src/main/java/edu/unimag/sgpm/control/dto/ParqueaderoDto.java
+++ src/main/java/edu/unimag/sgpm/control/dto/ParqueaderoDto.java
@@ -1,6 +1,8 @@
 package edu.unimag.sgpm.control.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
-public record ParqueaderoDto(Integer id, String nombre, List<String> espacios) {
+public record ParqueaderoDto(Integer id, String nombre, MultipartFile imagen, String ruta, List<String> espacios) {
 }

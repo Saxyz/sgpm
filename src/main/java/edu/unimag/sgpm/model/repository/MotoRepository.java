7b908848d5e1e3e2cd01f14d1594639b3487dee1
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-05-28 07:43:48 -0500

    [MOD] cambiando tipo de String a Integer

--- src/main/java/edu/unimag/sgpm/model/repository/MotoRepository.java
+++ src/main/java/edu/unimag/sgpm/model/repository/MotoRepository.java
@@ -8,9 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MotoRepository extends JpaRepository<Moto, String> {
-
-    Optional<Moto> findByIdMoto(String placa);
+public interface MotoRepository extends JpaRepository<Moto, Integer> {
 
     List<Moto> findByUsuarioIdUsuario(Integer id);
 }
